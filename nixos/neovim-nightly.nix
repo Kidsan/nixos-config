63e1b30109690b3884b3883d1409c611ef69d502
kidsan <8798449+Kidsan@users.noreply.github.com>
2022-09-12 20:20:10 +0200

    neovim: add overlay for nightly neovim

---
+++ nixos/neovim-nightly.nix
@@ -0,0 +1,9 @@
+{ config, lib, pkgs, modulesPath, ... }:
+
+{
+  nixpkgs.overlays = [
+    (import (builtins.fetchTarball {
+      url = https://github.com/nix-community/neovim-nightly-overlay/archive/master.tar.gz;
+    }))
+  ];
+}
