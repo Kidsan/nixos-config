44fce59512661ddf8fbf598210210dcadcfe6fb4
kidsan <8798449+Kidsan@users.noreply.github.com>
2022-11-11 13:24:41 +0100

    sway: fix screensharing

--- configuration/thinkpad.nix
+++ configuration/thinkpad.nix
@@ -19,8 +19,8 @@ let
 
     text = ''
   dbus-update-activation-environment --systemd WAYLAND_DISPLAY XDG_CURRENT_DESKTOP=sway
-  systemctl --user stop pipewire pipewire-media-session xdg-desktop-portal xdg-desktop-portal-wlr
-  systemctl --user start pipewire pipewire-media-session xdg-desktop-portal xdg-desktop-portal-wlr
+  systemctl --user stop pipewire wireplumber xdg-desktop-portal xdg-desktop-portal-wlr
+  systemctl --user start pipewire wireplumber xdg-desktop-portal xdg-desktop-portal-wlr
       '';
   };
 
