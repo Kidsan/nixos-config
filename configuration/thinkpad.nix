4c149cf402c3bac4040e468622ca637e090fdc15
kidsan <8798449+Kidsan@users.noreply.github.com>
2022-11-14 17:22:13 +0100

    thinkpad: add pulseaudio pkg to fix volume ctrl

--- configuration/thinkpad.nix
+++ configuration/thinkpad.nix
@@ -230,6 +230,7 @@ in
     wl-clipboard # wl-copy and wl-paste for copy/paste from stdin / stdout
     bemenu # wayland clone of dmenu
     mako # notification system developed by swaywm maintainer
+    pulseaudio
     # /SWAY
   ];
 
