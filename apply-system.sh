483acb59b159d1ba7d2d7ad173a80685ca2fbe38
kidsan <8798449+Kidsan@users.noreply.github.com>
2022-11-16 16:41:19 +0100

    home-manager: garbage collection on old generations

--- apply-system.sh
+++ apply-system.sh
@@ -3,5 +3,6 @@ pushd ~/nixos-config
 sudo nix-collect-garbage --delete-older-than 7d
 nix flake update --commit-lock-file
 sudo nixos-rebuild switch --flake .# --impure
+home-manager expire-generations "-7 days"
 home-manager switch --flake .#kidsan
 popd
