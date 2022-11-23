525dfb73ad43bfaed1cd478d1774f2e277f36957
kidsan <8798449+Kidsan@users.noreply.github.com>
2022-11-23 13:05:03 +0100

    lobster: home stateversion must be 20.09 or greater

--- home/lobster/home.nix
+++ home/lobster/home.nix
@@ -7,12 +7,11 @@
   home.username = "lobster";
   home.homeDirectory = "/home/lobster";
 
-  home.stateVersion = "20.03";
+  home.stateVersion = "20.09";
 
   home.packages = with pkgs; [
 
   ];
-
 
   programs.git = {
     enable = true;
@@ -20,24 +19,6 @@
     userEmail = "8798449+Kidsan@users.noreply.github.com";
     ignores = [ "*.nix" "flake.lock" "!personal/**/*.nix" "!personal/**/flake.lock" "!nixos-config/**/*.nix" "!nixos-config/**/flake.lock" ];
     extraConfig = { init = { defaultBranch = "main"; }; pull = { rebase = true; }; };
-  };
-
-  programs.neovim = {
-    enable = true;
-    package = pkgs.neovim-nightly;
-    vimAlias = true;
-    extraConfig = ''
-      set number
-
-      :map <Up> <Nop>
-      :map <Left> <Nop>
-      :map <Right> <Nop>
-      :map <Down> <Nop>
-    '';
-    plugins = with pkgs.vimPlugins; [
-      vim-nix
-      yankring
-    ];
   };
 
   programs.direnv = {
