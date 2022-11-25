281515ce18ac9040107e95a49d5099778690287c
kidsan <8798449+Kidsan@users.noreply.github.com>
2022-11-25 12:31:17 +0100

    bemenu: tyle bemenu to match waybar

--- home/kidsan/home.nix
+++ home/kidsan/home.nix
@@ -174,7 +174,7 @@
         { command = "sleep 5; systemctl --user start kanshi.service"; }
       ];
 
-      menu = "bemenu-run";
+      menu = "bemenu-run -H 30 --tb '#6272a4' --tf '#f8f8f2' --fb '#282a36' --ff '#f8f8f2' --nb '#282a36' --nf '#6272a4' --hb '#44475a' --hf '#50fa7b' --sb '#44475a' --sf '#50fa7b' --scb '#282a36' --scf '#ff79c6'";
 
       input = {
         "*" = { xkb_layout = "gb"; xkb_options = "caps:escape,compose:ralt"; };
