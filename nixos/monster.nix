fbb95aeac23594d18663e932d171caad334ca41a
kidsan <8798449+Kidsan@users.noreply.github.com>
2022-11-30 14:08:46 +0100

    ssh: update public key

--- nixos/monster.nix
+++ nixos/monster.nix
@@ -50,7 +50,7 @@
     home = "/home/lobster";
     extraGroups = [ "wheel" "networkmanager" ];
     openssh.authorizedKeys.keys = [
-      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDzGxz8p3VvP+WSSQEpsh8akVnqwrfJr6Se9BtRFIyqG kidsan@thinkpad"
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKas9qjJOceFVG6IS3LgH1RL0EBNZ66LFeLrsOqT31IL kidsan@thinkpad"
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPkgNbqSgAdMEx/IaXFsGW6HlobqrsSnl7lanbdfMYaZ JuiceSSH"
     ];
   };
