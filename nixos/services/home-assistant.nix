05956f7b580b80dc34fcd0c6afe0198b80278232
kidsan <8798449+Kidsan@users.noreply.github.com>
2022-12-15 15:07:31 +0100

    home-assistant: add home-assistant configuration
    This includes configuration for zwavejs server running in an oci container

--- nixos/services/home-assistant.nix
+++ nixos/services/home-assistant.nix
@@ -4,8 +4,35 @@
   services.home-assistant = {
     enable = true;
     extraComponents = [
-      "zwave_js"
+      "person"
+      "image"
+      "onboarding"
+      "frontend"
+      "cloud"
     ];
+    config = {
+      default_config = { };
+      frontend = { };
+      config = { };
+      mobile_app = { };
+      backup = { };
+      zwave_js = { };
+      automation = "!include automations.yaml";
+    };
+    configWritable = true;
     openFirewall = true;
+  };
+
+  virtualisation.oci-containers = {
+    backend = "docker";
+    containers.zwave_ui = {
+      volumes = [ "/var/lib/zwavejs/store:/usr/src/app/store" ];
+      image = "zwavejs/zwave-js-ui:latest";
+      ports = [ "3000:3000" "8091:8091" ];
+      autoStart = true;
+      extraOptions = [
+        "--device=/dev/ttyUSB0:/dev/zwave"
+      ];
+    };
   };
 }
