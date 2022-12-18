a008013436ec82578277fdc0a949c2a21df0aafa
kidsan <8798449+Kidsan@users.noreply.github.com>
2022-12-18 19:52:50 +0100

    thinkpad: enable bluetooth

--- nixos/thinkpad.nix
+++ nixos/thinkpad.nix
@@ -76,6 +76,8 @@
   # Enable CUPS to print documents.
   services.printing.enable = true;
 
+  hardware.bluetooth.enable = true;
+
   # Enable sound with pipewire.
   sound.enable = true;
   hardware.pulseaudio.enable = false;
