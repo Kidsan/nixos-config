f8517aa1bb41415b76c8608fe5772356a8684002
kidsan <8798449+Kidsan@users.noreply.github.com>
2022-12-19 16:04:59 +0100

    monster: update keys for monster

--- secrets/config.nix
+++ secrets/config.nix
@@ -1,10 +1,10 @@
 let
   kidsanThinkpad = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKas9qjJOceFVG6IS3LgH1RL0EBNZ66LFeLrsOqT31IL";
-  lobsterMonster = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFIIjC0jPPm97QPnB5vJi3k4l1B9SVl13u/CV2KoYqrD";
+  lobsterMonster = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMuW2Mi0u4NqXtTr4FSzN9b/qh5UlMGbo+JcqCqCfoP4 lobster@monster";
   users = [ kidsanThinkpad lobsterMonster ];
 
   thinkpad = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMdwwNmNX3e4ZNAUyuPWJcHkYCS03dPA1DexNJtKPgP4";
-  monster = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIFtqmq5ozAZcaMj5k1Jc64abBLC4h/czURw8sEYlWYD";
+  monster = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOcC1fi7RJkUsbF5tnonzCL0ki8VkGLrDq+BNli6tURo";
   systems = [ thinkpad monster ];
 in
 {
