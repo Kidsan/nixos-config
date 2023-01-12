b06211134c1d35f50671fb38a4902926866413af
kidsan <8798449+Kidsan@users.noreply.github.com>
2023-01-12 15:39:29 +0100

    nvim: lsp fixes

--- home/programs/neovim/nvim/after/plugin/lsp.lua
+++ home/programs/neovim/nvim/after/plugin/lsp.lua
@@ -40,7 +40,6 @@ lsp.on_attach(function(client, bufnr)
  vim.keymap.set("n", "<leader>vrr", vim.lsp.buf.references, opts)
  vim.keymap.set("n", "<leader>vrn", vim.lsp.buf.rename, opts)
  vim.keymap.set("i", "<C-h>", vim.lsp.buf.signature_help, opts)
-
  
  end)
 
