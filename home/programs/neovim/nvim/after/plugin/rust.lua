5c292f3ec132b7013c40d1ab64edcf877b03c7fa
kidsan <8798449+Kidsan@users.noreply.github.com>
2023-01-17 14:42:26 +0100

    rust: enable fmt on save

--- home/programs/neovim/nvim/after/plugin/rust.lua
+++ home/programs/neovim/nvim/after/plugin/rust.lua
@@ -1,5 +1,13 @@
 local rt = require("rust-tools")
 
+local format_sync_grp = vim.api.nvim_create_augroup("Format", {})
+vim.api.nvim_create_autocmd("BufWritePre", {
+  pattern = "*.rs",
+  callback = function()
+    vim.lsp.buf.format({ timeout_ms = 200 })
+  end,
+  group = format_sync_grp,
+})
 
 
 rt.setup({
