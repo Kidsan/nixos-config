271138d8240e017c2d3078e63ce02b2608732179
kidsan <8798449+Kidsan@users.noreply.github.com>
2023-01-18 17:43:30 +0100

    neovim(treesitter): add cool incremental selection
    Add keybind for normal mode that incrementally selects upwards from hovered item on <CR>

--- home/programs/neovim/nvim/after/plugin/treesitter.lua
+++ home/programs/neovim/nvim/after/plugin/treesitter.lua
@@ -5,4 +5,13 @@ require'nvim-treesitter.configs'.setup {
     enable = true,
     additional_vim_regex_highlighting = false,
   },
+  incremental_selection = {
+    enable = true,
+    keymaps = {
+      init_selection = "<CR>",
+      node_incremental = "<CR>",
+      scope_incremental = "<S-CR>",
+      node_decremental = "<BS>",
+    },
+  },
 }
