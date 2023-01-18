9cfedbc346085314576c69e21896c51480d68887
kidsan <8798449+Kidsan@users.noreply.github.com>
2023-01-18 11:09:48 +0100

    script: reorder operations

--- apply-system.sh
+++ apply-system.sh
@@ -1,9 +1,8 @@
 #!/bin/sh
 pushd ~/nixos-config
-sudo nix-collect-garbage --delete-older-than 7d
-# nix flake update --commit-lock-file
 git pull
 sudo nixos-rebuild switch --flake .# --impure
-home-manager expire-generations "-7 days"
 home-manager switch --flake .
+sudo nix-collect-garbage --delete-older-than 7d
+home-manager expire-generations "-7 days"
 popd
