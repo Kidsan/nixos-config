30cd45f05652a821736709da5e9de65c38a3535f
kidsan <8798449+Kidsan@users.noreply.github.com>
2023-01-19 10:48:07 +0100

    scripts: separate cleanup script

--- apply-system.sh
+++ apply-system.sh
@@ -3,6 +3,4 @@ pushd ~/nixos-config
 git pull
 sudo nixos-rebuild switch --flake .# --impure
 home-manager switch --flake .
-sudo nix-collect-garbage --delete-older-than 7d
-home-manager expire-generations "-7 days"
 popd
