fd32f8a5ef03a7c3a93c8cc6acc6f2afeef779a0
kidsan <8798449+Kidsan@users.noreply.github.com>
2023-02-15 18:28:29 +0100

    mako: add dracula theme for notifications

--- home/programs/sway.nix
+++ home/programs/sway.nix
@@ -128,6 +128,22 @@
 
   };
 
+  home.file = {
+    "./.config/mako/config".text = ''
+      background-color=#282a36
+      text-color=#44475a
+      border-color=#282a36
+
+      [urgency=low]
+      border-color=#282a36
+
+      [urgency=normal]
+      border-color=#f1fa8c
+
+      [urgency=high]
+      border-color=#ff5555
+    '';
+  };
   programs.mako = {
     enable = true;
     defaultTimeout = 5000;
