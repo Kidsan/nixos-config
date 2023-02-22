cdcafbcb23872d964bae2f2c31d3373f7306d20e
kidsan <8798449+Kidsan@users.noreply.github.com>
2023-02-22 09:56:08 +0100

    sway: window ordering

--- home/programs/sway.nix
+++ home/programs/sway.nix
@@ -68,9 +68,8 @@
       startup = [
         { command = "sleep 5; systemctl --user restart kanshi.service"; always = true; }
         { command = "firefox"; }
-        { command = "thunderbird"; }
         { command = "slack"; }
-        { command = "alacritty"; }
+        { command = "thunderbird"; }
       ];
 
       menu = "bemenu-run -H 30 --tb '#6272a4' --tf '#f8f8f2' --fb '#282a36' --ff '#f8f8f2' --nb '#282a36' --nf '#6272a4' --hb '#44475a' --hf '#50fa7b' --sb '#44475a' --sf '#50fa7b' --scb '#282a36' --scf '#ff79c6'";
@@ -112,7 +111,7 @@
 
       assigns = {
         "1" = [{ app_id = "firefox-nightly"; }];
-        "2" = [{ app_id = "Slack"; } { app_id = "thunderbird"; }];
+        "2" = [{ app_id = "thunderbird"; } { app_id = "Slack"; }];
         "4" = [{ class = "discord"; } { class = "Spotify"; }];
       };
     };
