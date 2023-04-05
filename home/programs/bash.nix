1cab6bcc160a5503c4a686c0d03f382b1a4d29f2
kidsan <8798449+Kidsan@users.noreply.github.com>
2023-04-05 14:43:05 +0200

    bash: git branch indicator in prompt

--- home/programs/bash.nix
+++ home/programs/bash.nix
@@ -6,6 +6,10 @@
   programs.bash.enable = true;
   programs.bash.bashrcExtra = ''
     export PATH=$PATH:~/go/bin:~/.cargo/bin
+    parse_git_branch() {
+      git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/\[(\1)\]/'
+    }
+    export PS1="\n\[\033[1;32m\][\[\e]0;\u@\h: \w\a\]\u@\h:\w]\[\033[33m\]\$(parse_git_branch)\[\033[1;32m\]\$\[\033[0m\] "
   '';
   programs.bash.shellAliases = {
     k = "kubectl";
