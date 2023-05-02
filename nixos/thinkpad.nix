a19ae7acad47e326ac10b26f40a8260d6024dfe8
kidsan <8798449+Kidsan@users.noreply.github.com>
2023-05-02 10:09:37 +0200

    nixos(kernel): move thinkpad to 6.3

--- nixos/thinkpad.nix
+++ nixos/thinkpad.nix
@@ -29,7 +29,7 @@
   boot.loader.efi.efiSysMountPoint = "/boot/efi";
   boot.binfmt.emulatedSystems = [ "aarch64-linux" "armv7l-linux" ];
 
-  boot.kernelPackages = pkgs.linuxPackages_6_2;
+  boot.kernelPackages = pkgs.linuxPackages_6_3;
 
   # Setup keyfile
   boot.initrd.secrets = {
