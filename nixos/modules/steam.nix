47f5ce45da297336148382b725459435a48cf14f
kidsan <8798449+Kidsan@users.noreply.github.com>
2023-06-09 11:53:42 +0200

    steam: fix missing dep

--- nixos/modules/steam.nix
+++ nixos/modules/steam.nix
@@ -5,5 +5,6 @@
     enable = true;
     remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
     dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
+    package = with pkgs; steam.override { extraPkgs = pkgs: [ attr ]; };
   };
 }
