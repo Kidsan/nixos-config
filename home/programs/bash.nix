053e3e9ff5f6c40fe2a86ce9adff62f6e556773a
kidsan <8798449+Kidsan@users.noreply.github.com>
2023-06-12 09:18:10 +0200

    fortunes: move to xdg_config_home

--- home/programs/bash.nix
+++ home/programs/bash.nix
@@ -14,13 +14,13 @@
       git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/\[(\1)\]/'
     }
     export PS1="\n\[\033[1;32m\][\[\e]0;\u@\h: \w\a\]\u@\h:\w]\[\033[33m\]\$(parse_git_branch)\[\033[1;32m\]\$\[\033[0m\] "
-    fortune ~/fortunes/futurama
+    fortune ~/.config/fortunes/futurama
   '';
   programs.bash.shellAliases = {
     k = "kubectl";
   };
 
-  home.file."./fortunes" = { source = ./fortunes; };
+  home.file.".config/fortunes" = { source = ./fortunes; };
 
   # programs.bash.sessionVariables = {
   # SSH_AGENT_PID="";
