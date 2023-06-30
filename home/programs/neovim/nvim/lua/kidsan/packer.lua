2ae358b0d1bd157e0772c27185a7227146a45fce
kidsan <8798449+Kidsan@users.noreply.github.com>
2023-06-30 10:26:56 +0200

    nvim: increase timeout for which-key popup

--- home/programs/neovim/nvim/lua/kidsan/packer.lua
+++ home/programs/neovim/nvim/lua/kidsan/packer.lua
@@ -98,7 +98,7 @@ return require('packer').startup(function(use)
         'folke/which-key.nvim',
         config = function()
             vim.o.timeout = true
-            vim.o.timeoutlen = 300
+            vim.o.timeoutlen = 1000
             require("which-key").setup {
 
             }
