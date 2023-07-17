c01ef9394663f4e77a2c7c2b3cbc97195290ee65
kidsan <8798449+Kidsan@users.noreply.github.com>
2023-07-17 18:47:54 +0200

    fix

--- home/programs/neovim/nvim/lua/plugins/lualine.lua
+++ home/programs/neovim/nvim/lua/plugins/lualine.lua
@@ -1,6 +1,6 @@
 return {
     {
         'nvim-lualine/lualine.nvim',
-        dependencies = { 'nvim-tree/nvim-web-devicons', opt = true }
+        dependencies = { 'nvim-tree/nvim-web-devicons', optional = true }
     }
 }
