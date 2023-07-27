091953127aceea85a5f599a8bb8b717976d1c531
kidsan <8798449+Kidsan@users.noreply.github.com>
2023-07-27 09:44:52 +0200

    fonts: rename fonts configuration

--- nixos/modules/fonts.nix
+++ nixos/modules/fonts.nix
@@ -2,7 +2,7 @@
 
 {
   fonts.fontDir.enable = true;
-  fonts.fonts = with pkgs; [
+  fonts.packages = with pkgs; [
     font-awesome # installed for waybar icons
     (nerdfonts.override { fonts = [ "JetBrainsMono" ]; })
   ];
