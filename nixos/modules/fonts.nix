87aa37ebd98b530eb634ffc432646783c6c6366a
kidsan <8798449+Kidsan@users.noreply.github.com>
2023-07-31 16:47:35 +0200

    fonts: fix font errors(finally)

--- nixos/modules/fonts.nix
+++ nixos/modules/fonts.nix
@@ -4,6 +4,7 @@
   fonts.fontDir.enable = true;
   fonts.packages = with pkgs; [
     noto-fonts
+    noto-fonts-cjk
     noto-fonts-emoji
     font-awesome # installed for waybar icons
     (nerdfonts.override { fonts = [ "JetBrainsMono" ]; })
