386c479ce9d8511848797d4d2768e0801ed45ffb
kidsan <8798449+Kidsan@users.noreply.github.com>
2023-08-16 13:03:20 +0200

    nvim: delete annoying keymap
    I keep accidentally making files executable

--- home/programs/neovim/nvim/lua/kidsan/remap.lua
+++ home/programs/neovim/nvim/lua/kidsan/remap.lua
@@ -27,4 +27,3 @@ vim.keymap.set("n", "<leader>k", "<cmd>lnext<CR>zz")
 vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
 
 vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
-vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
