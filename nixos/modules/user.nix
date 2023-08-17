73ae8c1bee8d066c837c0404415dd14d0f8d72e2
kidsan <8798449+Kidsan@users.noreply.github.com>
2023-08-17 17:05:10 +0200

    allow phone ssh key for ssh

--- nixos/modules/user.nix
+++ nixos/modules/user.nix
@@ -7,6 +7,7 @@ _:
     extraGroups = [ "networkmanager" "wheel" "docker" "video" ];
     openssh.authorizedKeys.keys = [
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKas9qjJOceFVG6IS3LgH1RL0EBNZ66LFeLrsOqT31IL kidsan@thinkpad"
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPkgNbqSgAdMEx/IaXFsGW6HlobqrsSnl7lanbdfMYaZ JuiceSSH"
     ];
   };
 }
