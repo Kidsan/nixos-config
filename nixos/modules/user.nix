e2a4dfcda33b043f03bc038d8b03264389359130
kidsan <8798449+Kidsan@users.noreply.github.com>
2023-08-22 17:44:17 +0200

    ssh from desktop

--- nixos/modules/user.nix
+++ nixos/modules/user.nix
@@ -8,6 +8,7 @@ _:
     openssh.authorizedKeys.keys = [
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKas9qjJOceFVG6IS3LgH1RL0EBNZ66LFeLrsOqT31IL kidsan@thinkpad"
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPkgNbqSgAdMEx/IaXFsGW6HlobqrsSnl7lanbdfMYaZ JuiceSSH"
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPUoDNMX11//LajxTS4G0Ndj84jwh1mxn38J4g1CULhN"
     ];
   };
 }
