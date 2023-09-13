77b6825ab93d3b1a7eb7e1ef83b0e315e1b613b8
kidsan <8798449+Kidsan@users.noreply.github.com>
2023-09-13 13:56:35 +0200

    nvim: add hydrate

---
+++ home/programs/neovim/nvim/lua/plugins/hydrate.lua
@@ -0,0 +1,10 @@
+return {
+    "stefanlogue/hydrate.nvim",
+    version = "*",
+    event = "VeryLazy",
+    opts = {
+        minute_interval = 20,
+        render_style = "compact",
+        persist_timer = true,
+    },
+}
