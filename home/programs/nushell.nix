4c02ce6da280a2e8f6a1b09535f1e7abecc4151f
kidsan <8798449+Kidsan@users.noreply.github.com>
2023-09-22 13:21:46 +0200

    starship: basic prompt

--- home/programs/nushell.nix
+++ home/programs/nushell.nix
@@ -3,4 +3,21 @@
   programs.nushell = {
     enable = true;
   };
+
+  programs.starship = {
+    enable = true;
+    enableBashIntegration = false;
+    enableNushellIntegration = true;
+    settings = {
+      add_newline = true;
+      character.success_symbol = "[➜](bold green)";
+      package.disabled = true;
+      nix_shell = {
+        format = "[$symbol]($style)";
+      };
+      rust = { };
+      golang = { format = "[$symbol($version )]($style)"; };
+      format = "$username$directory$git_branch$golang$rust$nix_shell$character";
+    };
+  };
 }
