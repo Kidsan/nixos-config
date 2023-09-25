af9df2c0eee5e65d10ccdab865722c27469d6558
kidsan <8798449+Kidsan@users.noreply.github.com>
2023-09-25 14:26:53 +0200

    nushell: add to system conf
    We get various errors if it isn't in /run/current-system/ when it is only installed by hm.

--- nixos/thinkpad.nix
+++ nixos/thinkpad.nix
@@ -38,6 +38,7 @@
   # $ nix search wget
   environment.systemPackages = with pkgs; [
     vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+    nushell
     wget
     slack
     thunderbird
