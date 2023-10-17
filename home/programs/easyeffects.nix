f1082b861f21f38404fdaff916d6b8a999ec195e
kidsan <8798449+Kidsan@users.noreply.github.com>
2023-10-17 22:37:53 +0200

    easyeffects: modify preset to fix autoloading

--- home/programs/easyeffects.nix
+++ home/programs/easyeffects.nix
@@ -8,11 +8,12 @@ _:
       {
           "input": {
               "blocklist": [],
-              "compressor": {
+              "compressor#0": {
                   "attack": 20.0,
                   "boost-amount": 6.0,
                   "boost-threshold": -72.0,
                   "bypass": false,
+                  "dry": -100.0,
                   "hpf-frequency": 10.0,
                   "hpf-mode": "off",
                   "input-gain": 0.0,
@@ -24,7 +25,7 @@ _:
                   "output-gain": 0.0,
                   "ratio": 4.0,
                   "release": 100.0,
-                  "release-threshold": -120.0,
+                  "release-threshold": -100.0,
                   "sidechain": {
                       "lookahead": 0.0,
                       "mode": "RMS",
@@ -33,9 +34,10 @@ _:
                       "source": "Middle",
                       "type": "Feed-forward"
                   },
-                  "threshold": -12.0
+                  "threshold": -12.0,
+                  "wet": 0.0
               },
-              "deesser": {
+              "deesser#0": {
                   "bypass": false,
                   "detection": "RMS",
                   "f1-freq": 6000.0,
@@ -52,20 +54,25 @@ _:
                   "sc-listen": false,
                   "threshold": -18.0
               },
-              "filter": {
+              "filter#0": {
+                  "balance": 0.0,
                   "bypass": false,
                   "frequency": 80.0,
-                  "inertia": 20.0,
+                  "gain": 0.0,
                   "input-gain": 0.0,
-                  "mode": "12dB/oct Highpass",
+                  "mode": "RLC (BT)",
                   "output-gain": 0.0,
-                  "resonance": -3.0
+                  "quality": 0.0,
+                  "slope": "x1",
+                  "type": "Low-pass",
+                  "width": 4.0
               },
-              "gate": {
+              "gate#0": {
                   "attack": 5.0,
                   "bypass": false,
                   "curve-threshold": -24.0,
                   "curve-zone": -6.0,
+                  "dry": -100.0,
                   "hpf-frequency": 10.0,
                   "hpf-mode": "off",
                   "hysteresis": false,
@@ -85,9 +92,10 @@ _:
                       "preamp": 0.0,
                       "reactivity": 10.0,
                       "source": "Middle"
-                  }
+                  },
+                  "wet": 0.0
               },
-              "limiter": {
+              "limiter#0": {
                   "alr": false,
                   "alr-attack": 5.0,
                   "alr-knee": 0.0,
@@ -108,18 +116,22 @@ _:
                   "threshold": -1.0
               },
               "plugins_order": [
-                  "gate",
-                  "compressor",
-                  "filter",
-                  "deesser",
-                  "rnnoise",
-                  "limiter"
+                  "gate#0",
+                  "compressor#0",
+                  "filter#0",
+                  "deesser#0",
+                  "rnnoise#0",
+                  "limiter#0"
               ],
-              "rnnoise": {
+              "rnnoise#0": {
                   "bypass": false,
+                  "enable-vad": false,
                   "input-gain": 0.0,
                   "model-path": "",
-                  "output-gain": 0.0
+                  "output-gain": 0.0,
+                  "release": 20.0,
+                  "vad-thres": 50.0,
+                  "wet": 0.0
               }
           }
       }
