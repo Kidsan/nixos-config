28a61156cf74a4a84c95582ee53665801690f721
kidsan <8798449+Kidsan@users.noreply.github.com>
2023-10-19 12:18:21 +0200

    scripts: remove unused scripts

--- apply-system.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-pushd ~/nixos-config
-git pull
-sudo nixos-rebuild switch --flake .#
-home-manager switch --flake .
-popd
--- cleanup.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-sudo nix-collect-garbage --delete-older-than 7d
-home-manager expire-generations "-7 days"
