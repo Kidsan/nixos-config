f6a03a8f48af636fba0c35e49fe710d01df6235f
kidsan <8798449+Kidsan@users.noreply.github.com>
2023-12-14 12:14:10 +0100

    git: enable diff3 conflictstyle

--- home/programs/git.nix
+++ home/programs/git.nix
@@ -24,6 +24,7 @@
       init = { defaultBranch = "main"; };
       pull = { rebase = true; };
       push = { autoSetupRemote = true; };
+      merge = { conflictstyle = "diff3"; };
     };
   };
 }
