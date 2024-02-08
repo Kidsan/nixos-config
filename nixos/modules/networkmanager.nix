ddb1a7e4e6942afa2ffe61ef8473b1e5264463a2
kidsan <8798449+Kidsan@users.noreply.github.com>
2024-02-08 16:23:21 +0100

    firewall: open ollama port

--- nixos/modules/networkmanager.nix
+++ nixos/modules/networkmanager.nix
@@ -5,4 +5,5 @@
   systemd.services.NetworkManager-wait-online.enable = lib.mkForce false;
   systemd.services.systemd-networkd-wait-online.enable = lib.mkForce false;
   systemd.network.wait-online.enable = false;
+  networking.firewall.allowedTCPPorts = [ 11434 ];
 }
