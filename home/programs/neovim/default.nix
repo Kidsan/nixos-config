6191582bc314cb46da9792d89a7e224c8fb3948b
kidsan <8798449+Kidsan@users.noreply.github.com>
2024-03-27 13:20:01 +0100

    nvim(treesitter): fix incorrectly linked grammars
    Should refactor this later to not list all the grammars twice.
    Or just use the allGrammars package

--- home/programs/neovim/default.nix
+++ home/programs/neovim/default.nix
@@ -28,6 +28,37 @@ let
     p.vue
     p.yaml
   ]));
+
+  treesitter-parsers = pkgs.symlinkJoin {
+    name = "treesitter-parsers";
+    paths = (pkgs.vimPlugins.nvim-treesitter.withPlugins (p: [
+      p.bash
+      p.comment
+      p.css
+      p.dockerfile
+      p.fish
+      p.gitattributes
+      p.gitignore
+      p.go
+      p.gomod
+      p.gowork
+      p.hcl
+      p.javascript
+      p.jq
+      p.json5
+      p.json
+      p.lua
+      p.make
+      p.markdown
+      p.nix
+      p.python
+      p.rust
+      p.toml
+      p.typescript
+      p.vue
+      p.yaml
+    ])).dependencies;
+  };
 in
 {
   home.packages = with pkgs; [
@@ -54,6 +85,12 @@ in
     source = ./nvim;
     recursive = true;
   };
+
+  home.file."./.config/nvim/lua/kidsan/init.lua".text = ''
+    require("kidsan.set")
+    require("kidsan.remap")
+    vim.opt.runtimepath:append("${treesitter-parsers}")
+  '';
 
   # Treesitter is configured as a locally developed module in lazy.nvim
   # we hardcode a symlink here so that we can refer to it in our lazy config
--- home/programs/neovim/nvim/lua/kidsan/init.lua
+++
@@ -1,2 +0,0 @@
-require("kidsan.set")
-require("kidsan.remap")
