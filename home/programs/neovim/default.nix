125dd9f4eab4acb6e366a8eaf52cac8ab5227bcb
kidsan <8798449+Kidsan@users.noreply.github.com>
2024-03-28 13:35:55 +0100

    nvim(treesitter): remove duplication of grammars

--- home/programs/neovim/default.nix
+++ home/programs/neovim/default.nix
@@ -31,33 +31,7 @@ let
 
   treesitter-parsers = pkgs.symlinkJoin {
     name = "treesitter-parsers";
-    paths = (pkgs.vimPlugins.nvim-treesitter.withPlugins (p: [
-      p.bash
-      p.comment
-      p.css
-      p.dockerfile
-      p.fish
-      p.gitattributes
-      p.gitignore
-      p.go
-      p.gomod
-      p.gowork
-      p.hcl
-      p.javascript
-      p.jq
-      p.json5
-      p.json
-      p.lua
-      p.make
-      p.markdown
-      p.nix
-      p.python
-      p.rust
-      p.toml
-      p.typescript
-      p.vue
-      p.yaml
-    ])).dependencies;
+    paths = treesitterWithGrammars.dependencies;
   };
 in
 {
