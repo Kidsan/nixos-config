a39e38b3493f387f633149eb4b21e3782231369e
kidsan <8798449+Kidsan@users.noreply.github.com>
2024-09-02 00:11:14 +0200

    sway: start kdeconnect-indicator

--- home/programs/sway.nix
+++ home/programs/sway.nix
@@ -95,6 +95,7 @@
         { command = "obsidian"; always = true; }
         { command = "alacritty -e ncspot"; }
         { command = "firefox"; }
+	{ command = "kdeconnect-indicator"; }
       ];
 
       menu = "bemenu-run -H 30 --tb '#6272a4' --tf '#f8f8f2' --fb '#282a36' --ff '#f8f8f2' --nb '#282a36' --nf '#6272a4' --hb '#44475a' --hf '#50fa7b' --sb '#44475a' --sf '#50fa7b' --scb '#282a36' --scf '#ff79c6'";
