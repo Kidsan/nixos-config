6c07957d5c401463637aeac50cba3452c2d40cf2
kidsan <8798449+Kidsan@users.noreply.github.com>
2024-09-05 19:20:05 +0200

    sway: add keybind for screenshotting to clipboard

--- home/programs/sway.nix
+++ home/programs/sway.nix
@@ -141,6 +141,7 @@
           "XF86AudioLowerVolume" = "exec 'wpctl set-volume @DEFAULT_AUDIO_SINK@ 2%- -l 1.0'";
           "XF86AudioMute" = "exec 'wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle'";
           "Print" = "exec 'FILENAME=\"screenshot-`date +%F-%T`\"; grim -g \"$(slurp)\" ~/Downloads/$FILENAME.png '";
+	  "${modifier}+Print" = "exec 'grim -g \"$(slurp -d)\" - | wl-copy";
           "${modifier}+period" = "exec 'playerctl -p ncspot next'";
           "${modifier}+comma" = "exec 'playerctl -p ncspot previous'";
           "${modifier}+shift+g" = "exec '~/.config/sway/mode_gaming.sh'";
