69517bc150f2892a0069dd210039395d1311178e
kidsan <8798449+Kidsan@users.noreply.github.com>
2024-09-05 20:16:09 +0200

    sway: remove warning about theme

--- home/programs/sway.nix
+++ home/programs/sway.nix
@@ -43,7 +43,7 @@
       name = "Dracula";
     };
     cursorTheme = {
-      package = pkgs.gnome3.adwaita-icon-theme;
+      package = pkgs.adwaita-icon-theme;
       name = "Adwaita";
     };
     font = {
