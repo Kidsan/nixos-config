9ca530dd4b81d276bb5f50149c96b99641f48018
kidsan <8798449+Kidsan@users.noreply.github.com>
2024-09-21 19:59:37 +0200

    nvim: disable semanticTokensProvider

--- home/programs/neovim/nvim/lua/plugins/lsp.lua
+++ home/programs/neovim/nvim/lua/plugins/lsp.lua
@@ -98,7 +98,7 @@ return {
                 group = format_sync_grp,
             })
 
-            local lsp_attach = function(_, bufnr)
+            local lsp_attach = function(client, bufnr)
                 local opts = { buffer = bufnr, remap = false }
                 vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
                 vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
@@ -111,6 +111,7 @@ return {
                 vim.keymap.set("n", "<leader>vrr", vim.lsp.buf.references, opts)
                 vim.keymap.set("n", "<leader>vrn", vim.lsp.buf.rename, opts)
                 vim.keymap.set("i", "<C-h>", vim.lsp.buf.signature_help, opts)
+                client.server_capabilities.semanticTokensProvider = nil
             end
 
 
