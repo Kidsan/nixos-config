9bf2377da65b77b5fa147cacf59fec3f6bada872
kidsan <git@kidsan.dev>
2024-11-11 14:27:42 +0100

    fonts: update renamed font

--- nixos/modules/fonts.nix
+++ nixos/modules/fonts.nix
@@ -4,7 +4,7 @@
   fonts.fontDir.enable = true;
   fonts.packages = with pkgs; [
     noto-fonts
-    noto-fonts-cjk
+    noto-fonts-cjk-sans
     noto-fonts-emoji
     font-awesome # installed for waybar icons
     (nerdfonts.override { fonts = [ "JetBrainsMono" ]; })
