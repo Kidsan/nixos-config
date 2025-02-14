d3b3191905265e6c9d2af8b1377a6a654b5e7750
kidsan <git@kidsan.dev>
2025-02-14 01:05:54 +0100

    fix: enable cuda support for sunshine
    Somehow this works.

--- nixos/modules/sunshine.nix
+++ nixos/modules/sunshine.nix
@@ -1,8 +1,9 @@
-{ ... }:
+{ pkgs, ... }:
 
 {
   services.sunshine = {
     enable = true;
+    package = pkgs.sunshine.override { cudaSupport = true; };
     openFirewall = true;
     capSysAdmin = true;
   };
