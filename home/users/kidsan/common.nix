fc4dd55dd4f31fc0c6173528f17cad089f991d1d
kidsan <git@kidsan.dev>
2025-03-14 09:54:06 +0100

    fix: remove outdated electron override

--- home/users/kidsan/common.nix
+++ home/users/kidsan/common.nix
@@ -1,12 +1,4 @@
 { config, lib, pkgs, ... }:
-let
-  obsidian = pkgs.obsidian.override {
-    electron = pkgs.electron_24.overrideAttrs (_: {
-      preFixup = "patchelf --add-needed ${pkgs.libglvnd}/lib/libEGL.so.1 $out/bin/electron"; # NixOS/nixpkgs#272912
-      meta.knownVulnerabilities = [ ]; # NixOS/nixpkgs#273611
-    });
-  };
-in
 {
   # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
