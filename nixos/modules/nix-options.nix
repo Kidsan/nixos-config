8717244c06bdbaf07b83f0cd5f79c82f34a46fe4
kidsan <git@kidsan.dev>
2025-04-25 17:40:03 +0200

    raspberrypi: add cache

--- nixos/modules/nix-options.nix
+++ nixos/modules/nix-options.nix
@@ -31,10 +31,12 @@
         "https://nix-community.cachix.org"
         "https://nixpkgs-wayland.cachix.org"
         "https://cache.nixos.org/"
+        "https://nixos-raspberrypi.cachix.org"
       ];
       trusted-public-keys = [
         "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
         "nixpkgs-wayland.cachix.org-1:3lwxaILxMRkVhehr5StQprHdEo4IrE8sRho9R9HOLYA="
+        "nixos-raspberrypi.cachix.org-1:4iMO9LXa8BqhU+Rpg6LQKiGa2lsNh/j2oiYLNOQ5sPI="
       ];
     };
   };
